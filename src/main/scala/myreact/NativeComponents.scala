04617e25e8d668a0e7cac84c776fa3624ff8d6fd
Tin Pavlinic <Tin.Pavlinic@team.telstra.com>
2018-09-20 11:22:43 +1000

    Text component

--- src/main/scala/myreact/NativeComponents.scala
+++ src/main/scala/myreact/NativeComponents.scala
@@ -76,6 +76,48 @@ object NativeComponents {
     def apply(p: Props) = Component(p)
   }
 
+  object Text {
+    @JSImport("react-native", "Text")
+    @js.native
+    object RawComponent extends js.Object
+
+    val Js = JsComponent[JsProps, Children.Varargs, Null](RawComponent)
+    val Component = Js.cmapCtorProps[Props](_.toJs)
+
+    case class Props(style: Style) {
+      def toJs: JsProps = {
+        val p = new js.Object().asInstanceOf[JsProps]
+        p.style = style.toJs
+        p
+      }
+    }
+
+    @js.native
+    trait JsProps extends js.Object {
+      var style: JsStyle = js.native
+    }
+
+    case class Style(color: Color = Color.Black, fontWeight: FontWeight = FontWeight.Normal, fontSize: Int = 10) {
+      def toJs: JsStyle = {
+        val s = new js.Object().asInstanceOf[JsStyle]
+        s.color = color.toString()
+        s.fontWeight = fontWeight.toJs
+        println(s)
+        s
+      }
+    }
+
+    @js.native
+    trait JsStyle extends js.Object {
+      var color: String
+      var fontWeight: String
+    }
+    def apply(p: Props)(children: VdomNode*) = Component(p)(children: _*)
+  }
+
+
+
+
   object View {
     @JSImport("react-native", "View")
     @js.native
@@ -96,5 +138,14 @@ object NativeComponents {
     }
 
     def apply(p: Props)(children: VdomNode*) = Component(p)(children: _*)
+  }
+
+  sealed trait FontWeight {
+    val toJs: String
+  }
+
+  object FontWeight {
+    case object Normal extends FontWeight { val toJs = "normal" }
+    case object Bold extends FontWeight { val toJs = "bold" }
   }
 }
